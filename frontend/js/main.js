6759c9ec4e25ee30b994d50ce3f6a6257d14706b
Alexandre Vitorino <alexandre.vitorino00@gmail.com>
2026-04-28 14:35:32 -0300

    Adição de legentas, preços e correções no CSS

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -83,3 +83,17 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 reveals.forEach(el => observer.observe(el));
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
